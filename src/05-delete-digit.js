35cfc5888f48181c994fbce06661b9761f2be496
lyolya <Lelkabygr@mail.ru>
2021-04-12 07:33:58 +0400

    feat: added solution deleteDigit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  if (!n) return;
+
+  const array = n.toString().split('').map((item) => +item);
+  let valueMin = [...array].sort((a, b) => a - b)[0];
+  // eslint-disable-next-line consistent-return
+  return +array.map((item) => {
+    if (item === valueMin) {
+      valueMin = '';
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return item;
+  }).filter((item) => item !== undefined).join('');
 }
 
 module.exports = deleteDigit;
